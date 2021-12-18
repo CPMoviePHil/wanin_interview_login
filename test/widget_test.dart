875f24670b8b00de71c0f9a8cffd4795a4f1de86
黃智輝 <cpmoviephil@gmail.com>
2021-12-18 21:24:15 +0800

    移除註解

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wanin_interview_login/main.dart';
+import 'package:wanin_interview_login/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
